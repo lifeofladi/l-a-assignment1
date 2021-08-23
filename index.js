8f123d3a12b072a6f5013439066e837609b0b9d7
lifeofladi <dev@lifeofladi.com>
2021-08-23 00:08:05 -0700

    add many divs to DOM

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 console.log("connected!!!");
+//User Object
+let user = {
+  username: "somefunc",
+  age: generateAge(),
+  description: "some description",
+  img: "https://picsum.photos/50",
+};
+
+function creatUserElement() {
+  //Creating Divs
+  const containerDiv = document.createElement("div");
+  const imgElement = document.createElement("img");
+  const usenameElement = document.createElement("h3");
+  const ageElement = document.createElement("p");
+  const descElement = document.createElement("p");
+  //Setting Attributes
+  containerDiv.setAttribute("class", "user-container");
+  imgElement.setAttribute("class", "profile-img");
+  imgElement.setAttribute("src", user.img);
+  usenameElement.setAttribute("class", "username");
+  //Setting InnetHTML
+  usenameElement.innerHTML = user.username;
+  ageElement.innerHTML = user.age;
+  descElement.innerHTML = user.description;
+
+  let childElements = [imgElement, usenameElement, ageElement, descElement];
+  appendChildren(containerDiv, childElements);
+
+  return containerDiv;
+}
+
+//Append Children helper function
+function appendChildren(parent, children) {
+  for (let ele of children) {
+    parent.appendChild(ele);
+  }
+  return parent;
+}
+//Generates 100 users
+function createUserElements() {
+  for (let i = 1; i <= 100; i++) {
+    document.body.appendChild(creatUserElement());
+  }
+}
+
+for (let i = 1; i <= 100; i++) {
+  document.body.appendChild(creatUserElement());
+}
+
+// createUserElements();
+// console.log(containerDiv);
+
+//Helper funtion to set multiple attributes to an element
+function setAttributes(element, attr) {
+  for (let key in attr) {
+    el.setAttribute(key, attr[key]);
+  }
+}
+//generates random number
+function generateAge() {
+  //returns numbers between 18 and 89 that represent user's age
+  return Math.floor(Math.random() * (90 - 18) + 18);
+}
+
+// console.log(generateAge());
+
+//Generate random username
+
+//Generate random description
+
+//Generate random users
